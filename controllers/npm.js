6ac8aae06633426124f19d3730e70b2791279044
Matt Hernandez <matt@modulus.io>
2013-11-08 19:28:18 -0500

    Added ideas on the npm controller.

---
+++ controllers/npm.js
@@ -0,0 +1,22 @@
+//
+//     npm-pulse
+//     Copyright(c) 2013 Applejacks
+//     Charlie Key, Taron Foxworth, Matt Hernandez
+//     MIT Licensed
+//
+
+module.exports = function() {
+  //
+  // There needs to be a way to grab module data in a structured format:
+  //
+  //    {
+  //      name: 'module-name'
+  //    , maintainers: ['some person']
+  //    , keywords: ['words']
+  //    , repository: 'git://github.com/user/repo.git'
+  //    , homepage: 'http://example.com/module-name'
+  //    , starredBy: ['maybe']
+  //    , readme: 'stringified readme.. maybe'
+  //    }
+  //
+};
