2fe70ac62859d86cf18982c40279eb1b18688fc2
Charlie Key <ckey@paranoidferret.com>
2013-11-10 13:45:11 -0500

    added getting module download data to npm controller

--- controllers/npm.js
+++ controllers/npm.js
@@ -5,10 +5,12 @@
 //     MIT Licensed
 //
 
-var request = require('request');
+var request = require('request')
+  , moment = require('moment');
 
 const NPM_ROOT_URL = 'http://registry.npmjs.org/';
-
+const DOWNLOAD_ROOT_URL = 'http://isaacs.iriscouch.com/';
+const DOWNLOAD_PKG_VIEW = 'downloads/_design/app/_view/pkg'
 //
 // npm constructor for working with module data from http://npmjs.org.
 //
@@ -23,6 +25,38 @@ Npm.prototype.getModule = function(name, fn) {
   var opts = {
     methid: 'GET'
   , url: NPM_ROOT_URL + name
+  , json: true
+  };
+
+  return request(opts, function(err, response, body) {
+    if (err) return fn(err);
+    if (response.statusCode !== 200) return fn(new Error(body));
+    fn(null, body);
+  });
+};
+
+/**
+ * Downloads the historical data for downloads off of couchdb.
+ * @param  {string}   name The package name to pull downloads.
+ * @param  {date}   start The start date for the data.
+ * @param  {date}   end The end date for the data.
+ * @param  {boolean}  total Whether or not to total values.
+ * @param  {Function} fn The callback
+ */
+Npm.prototype.getModuleDownloads = function(name, start, end, total, fn) {
+  var startStr = moment(start).format('YYYY-MM-DD');
+  var endStr = moment(end).format('YYYY-MM-DD');
+
+  var qs = {
+    startkey: JSON.stringify([name, startStr])
+  , endkey: JSON.stringify([name, endStr])
+  , group_level: total ? 1 : 2
+  };
+
+  var opts = {
+    methid : 'GET'
+  , url: DOWNLOAD_ROOT_URL + DOWNLOAD_PKG_VIEW
+  , qs: qs
   , json: true
   };
 
--- test.js
+++ test.js
@@ -8,3 +8,16 @@ Q.nfcall(controllers.npm.getModule, 'modulus')
   .then(console.log.bind(console, 'success:'))
   .fail(console.error.bind(console, 'failure:'))
   .done();
+
+var startDate = new Date(2013,10,1,0,0,0,0);
+var endDate = new Date();
+
+Q.nfcall(controllers.npm.getModuleDownloads, 'modulus', startDate, endDate, false)
+  .then(console.log.bind(console, 'success:'))
+  .fail(console.error.bind(console, 'failure:'))
+  .done();
+
+Q.nfcall(controllers.npm.getModuleDownloads, 'modulus', startDate, endDate, true)
+  .then(console.log.bind(console, 'success:'))
+  .fail(console.error.bind(console, 'failure:'))
+  .done();
