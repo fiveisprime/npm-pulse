192e85db556e7debea838ea26119960378dc1e0c
Charlie Key <ckey@paranoidferret.com>
2013-11-10 16:22:25 -0500

    added d3 and nvd3

--- api/assets/js/npm-pulse.js
+++ api/assets/js/npm-pulse.js
@@ -1,4 +1,4 @@
-angular.module('npm-pulse', ['ngRoute']).
+var npmPulse = angular.module('npm-pulse', ['ngRoute']).
   factory('Projects', function($q, $timeout) {
     var getProject = function(projectName) {
 
@@ -29,7 +29,7 @@ angular.module('npm-pulse', ['ngRoute']).
       when('/:projectName', {controller:ProjectCtrl, templateUrl:'/templates/project.html'}).
       otherwise({redirectTo:'/'});
   });
- 
+
 function IndexCtrl($scope, $location) {
   $scope.getProject = function() {
     if($scope.projectName){
@@ -37,7 +37,7 @@ function IndexCtrl($scope, $location) {
     }
   };
 }
- 
+
 
 function ProjectCtrl($scope, $location, $routeParams, Projects) {
 
@@ -52,8 +52,37 @@ function ProjectCtrl($scope, $location, $routeParams, Projects) {
       $location.path('/' + $scope.projectName);
     }
   };
-  
+
 }
 
+npmPulse.directive('moduleDownloadVis', function() {
 
- 
+  var margin = 20,
+    width = 960,
+    height = 500 - .5 - margin,
+    color = d3.interpolateRgb("#f77", "#77f");
+
+  return {
+    restrict: 'E',
+    scope: {
+      val: '='
+    },
+    link: function (scope, element, attrs) {
+      console.log('download vis');
+      console.log(element[0]);
+
+      var vis = d3.select(element[0])
+        .append("svg")
+          .attr("width", width)
+          .attr("height", height + margin + 100);
+
+      scope.$watch('val', function (newVal, oldVal) {
+        console.log('vis value set');
+
+        if (!newVal) {
+          return;
+        }
+      });
+    }
+  };
+});
