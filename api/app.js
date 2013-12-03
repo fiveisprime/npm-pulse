bbb4fb9dc6a7d7f4878de9099a0654677f5cbbf7
Charlie Key <ckey@paranoidferret.com>
2013-12-02 23:39:35 -0800

    Update app.js

--- api/app.js
+++ api/app.js
@@ -1,2 +1,4 @@
 // Start sails and pass it command line arguments
-require('sails').lift(require('optimist').argv);
+require('sails').lift(require('lodash').merge(require('optimist').argv, {
+  appPath: require('path').resolve(__dirname)
+});
