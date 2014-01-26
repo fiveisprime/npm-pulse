4086a06e2cd51d2b15229b1b81a5a4d21c575743
Taron Foxworth <taronfoxworth@gmail.com>
2014-01-26 12:36:04 -0500

    Added cacheing

--- site/app.js
+++ site/app.js
@@ -2,7 +2,9 @@ var express = require('express'),
   engine = require('ejs-locals'),
   app = express(),
   pulse = require('./../lib')(),
-  Q = require('q');
+  Q = require('q'),
+  TinyCache = require('tinycache'),
+  cache = new TinyCache();
 
 app.engine('ejs', engine);
 app.set('views', __dirname + '/views');
@@ -21,23 +23,34 @@ app.locals({
 
 
 app.get('/api/:projectName', function(req, res) {
+
   var projectName = req.param('projectName');
 
-  Q.nfcall(pulse.npm.getModule, projectName)
-    .then(function(moduleMeta) {
-      return Q.nfcall(pulse.gitHub.getRepo, moduleMeta);
-    })
-    .then(function(projectData) {
-      res.json(projectData);
-    })
-    .fail(function(err) {
-      console.error(err);
-      res.json({
-        fail: true,
-        error: err.message
-      });
-    })
-    .done();
+  var projectCache = cache.get(projectName);
+
+  if (projectCache) {
+    res.json(projectCache);
+  } else {
+    Q.nfcall(pulse.npm.getModule, projectName)
+      .then(function(moduleMeta) {
+        return Q.nfcall(pulse.gitHub.getRepo, moduleMeta);
+      })
+      .then(function(projectData) {
+        console.log(projectCache);
+        cache.put(projectName, projectData);
+        res.json(projectData);
+      })
+      .fail(function(err) {
+        console.error(err);
+        res.json({
+          fail: true,
+          error: err.message
+        });
+      })
+      .done();
+  }
+
+
 });
 
 app.all('/*', function(req, res) {
